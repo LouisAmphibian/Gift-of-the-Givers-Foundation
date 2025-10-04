083cca965e7235af916597a2ce6c182a48089c15
ayanda <ayandamantshinga30@gmail.com>
2025-10-04 11:15:13 +0200

    The databae class

---
+++ Gift-of-the-Givers Foundation/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Gift_of_the_Givers_Foundation.Models;
+
+namespace Gift_of_the_Givers_Foundation.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
